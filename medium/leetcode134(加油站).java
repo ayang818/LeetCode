5230568f0bd07e171850f33499a150f9a71af8a5
杨丰畅 <ayang818@qq.com>
2019-05-17 12:52:33 +0800

    添加三道贪心题

---
+++ medium/leetcode134(加油站).java
@@ -0,0 +1,32 @@
+    import java.util.Arrays;
+
+    class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int allGas = 0;
+            for (int i = 0; i < gas.length; i++) {
+                allGas+=gas[i];
+            }
+            int allCost = 0;
+            for (int i = 0; i < cost.length; i++) {
+                allCost+=cost[i];
+            }
+            if (allGas < allCost) return -1;
+            else{
+                int tempGas = 0;
+                int tempCost = 0;
+                int local = 0;
+                for (int i = 0; i < gas.length; i++) {
+                    tempCost+=cost[i];
+                    tempGas+=gas[i];
+                    // if (tempCost <= tempGas && i == gas.length-1) return local;
+                    if (tempCost > tempGas)
+                    {
+                        tempCost = 0;
+                        tempGas = 0;
+                        local=i+1;
+                    } 
+                }
+                return local;
+            }
+        }
+    }
