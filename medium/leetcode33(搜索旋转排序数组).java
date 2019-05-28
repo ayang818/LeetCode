64f7f6d5e33c56c6a72eb75b1a542058c6433fd7
ayang818 <ayang818@qq.com>
2019-05-28 16:07:53 +0800

    旋转数组二叉搜索

---
+++ medium/leetcode33(搜索旋转排序数组).java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && nums[mid] >= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+}
+}
