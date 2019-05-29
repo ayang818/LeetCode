f9369eebdd46b05e3e2f894c93211d1ab4d88470
ayang818 <ayang818@qq.com>
2019-05-29 16:32:49 +0800

    修改位运算方式

--- medium/leetcode29(两数相除).java
+++ medium/leetcode29(两数相除).java
@@ -11,33 +11,36 @@ class Solution {
 
         /** 符号位的处理参考了大佬的异或处理方法*/
 
-        boolean negetive= (dividend^ divisor)<0;
-        int res=0, div_count=1;
-        long dividend_tmp= Math.abs((long)dividend);
-        long divisor_tmp= Math.abs((long)divisor);
+        boolean negetive= (dividend ^ divisor)<0;
+        // count计数现在除数是开始的几倍
+        int res=0, count=1;
+        long tempDividend = Math.abs((long)dividend);
+        long tempDivisor = Math.abs((long)divisor);
         
-        while (dividend_tmp>= divisor_tmp) {
+        while (tempDividend >= tempDivisor) {
             // 题目不让使用乘除取模运算符
-            dividend_tmp -= divisor_tmp;
-            res += div_count;
-            
-            if (dividend_tmp< Math.abs(divisor))
+            tempDividend -= tempDivisor;
+            res += count;
+
+            if (tempDividend < Math.abs(divisor))
                 break;
             
-            if (dividend_tmp- divisor_tmp< divisor_tmp) {
-                divisor_tmp= Math.abs(divisor);
-                div_count=1;
+            if (tempDividend - tempDivisor < tempDivisor) {
+                // System.out.println(count);
+                // System.out.println(res);
+                tempDivisor = Math.abs(divisor);
+                count = 1;
                 continue;
             }
-            divisor_tmp+= divisor_tmp;
-            div_count+= div_count;
-        }
 
-        return negetive? 0-res: res;
+            tempDivisor += tempDivisor;
+            count += count;
+        }
+        return negetive ? 0-res: res;
     }
     public static void main(String[] args) {
         Solution s= new Solution();
-        int ans = s.divide(Integer.MIN_VALUE, 1);
+        int ans = s.divide(37, 2);
         System.out.println(ans);
     }
 }
