d8cab9b240b1efa9432fce6c40a5d88ce0dc5903
杨丰畅 <ayang818@qq.com>
2019-12-08 19:27:42 +0800

    添加反转链表

---
+++ easy/leetcode206(反转链表).java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ListNode node;
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next != null) {
+            ListNode tempNode = head.next;
+            head.next = node;
+            node = head;
+            return reverseList(tempNode);
+        }
+        head.next = node;
+        return head;
+    }
+}
