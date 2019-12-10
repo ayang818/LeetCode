d87cb5a0450bbd78d9efdd4ac051a84153ac06df
ayang818 <ayang818@qq.com>
2019-12-10 14:33:38 +0800

    使用普通方法完成一道双指针

---
+++ medium/leetcode3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength  = 0;
+        StringBuilder currentStringBuilder = new StringBuilder();
+        String currentString;
+        char[] charList = s.toCharArray();
+        for (int i = 0; i < charList.length ; i++ ) {
+            char currentChar = charList[i];
+            currentString = currentStringBuilder.toString();
+            char[] charList1 = currentString.toCharArray();
+            for (int j = 0; j < charList1.length; j++) {
+                char innerChar = charList1[j];
+                // 假设当前字符串在取出字符串中
+                if (innerChar == currentChar) {
+                    currentStringBuilder.delete(0, j + 1);
+                    break;
+                }
+            }
+            currentStringBuilder.append(currentChar);
+            int length = currentStringBuilder.toString().length();
+            if (length > maxLength) {
+                maxLength = length;
+            }
+        }
+        return maxLength;
+    }
+}
