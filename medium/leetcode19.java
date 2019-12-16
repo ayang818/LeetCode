60d10783a75acf0698f4c675985fec5aaa39287c
ayang818 <ayang818@qq.com>
2019-12-17 00:32:02 +0800

    双指针真的可以优化好多问题呀

---
+++ medium/leetcode19.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode left = dummy;
+        ListNode right = dummy;
+        if (head.next == null) {
+            return null;
+        }
+        int count = 0;
+        while (right != null) {
+            if (count <= n) {
+                right = right.next;
+            } else {
+                right = right.next;
+                left = left.next;
+            }
+            count++;
+        }
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
