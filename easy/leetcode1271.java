62a8e6f05187797b7a2666d01a42dafd36151383
杨丰畅 <ayang818@qq.com>
2019-12-28 15:58:33 +0800

    思维题

---
+++ easy/leetcode1271.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostToMoveChips(int[] chips) {
+        int ji = 0;
+        int ou = 0;
+        for (int i = 0; i< chips.length ; i++) {
+            if ((chips[i] & 1) == 1) {
+                ji+=1;
+            }else {
+                ou+=1;
+            }
+        }
+        return ji > ou ? ou : ji;
+    }
+}
