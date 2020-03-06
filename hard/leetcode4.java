5344fd5a2efd7081fcf4b9ea6fb0a14ee84672ff
杨丰畅 <ayang818@qq.com>
2020-03-06 17:52:24 +0800

    Update leetcode4 Solution to O(log(n))

--- hard/leetcode4.java
+++ hard/leetcode4.java
@@ -1,60 +1,48 @@
-import java.util.Arrays;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] concatList = new int[nums1.length + nums2.length];
-        for (int i = 0; i < nums1.length; i++) {
-            concatList[i] = nums1[i];
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
+        return left == right ? findKth(nums1, 0, nums2, 0, left) : (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
+    }
+    // i: nums1的起始位置 j: nums2的起始位置
+    public int findKth(int[] nums1, int i, int[] nums2, int j, int k){
+        // nums1为空数组
+        if( i >= nums1.length) return nums2[j + k - 1];
+        // nums2为空数组
+        if( j >= nums2.length) return nums1[i + k - 1];
+        if(k == 1){
+            return Math.min(nums1[i], nums2[j]);
         }
-        for (int i = 0; i < nums2.length; i++) {
-            concatList[nums1.length + i] = nums2[i];
+        int midVal1 = (i + k / 2 - 1 < nums1.length) ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+        int midVal2 = (j + k / 2 - 1 < nums2.length) ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
+        if(midVal1 < midVal2){
+            return findKth(nums1, i + k / 2, nums2, j , k - k / 2);
+        }else{
+            return findKth(nums1, i, nums2, j + k / 2 , k - k / 2);
         }
-        Arrays.sort(concatList);
-        int length = concatList.length;
-        if (length % 2 == 0) {
-            return (concatList[length / 2] + concatList[length / 2 - 1]) / 2.0;
-        }
-        return (concatList[length / 2]);
     }
 }
 
-class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int length1 = nums1.length;
-        int length2 = nums2.length;
-
-        if (length1 == 0) {
-            return length2 % 2 == 0 ? (nums2[(length2 / 2) - 1] + nums2[length2 / 2]) / 2.0 : nums2[length2 / 2];
-        }
-        if (length2 == 0) {
-            return length1 % 2 == 0 ? (nums1[(length1 / 2) - 1] + nums1[length1 / 2]) / 2.0 : nums1[length1 / 2];
-        }
-        int length = length1 + length2;
-        boolean isOdd = (length % 2 == 0);
-        int num1Local = 0;
-        int num2Local = 0;
-        int i1 = length / 2 + 1;
-        int[] ansList = new int[i1];
-        for (int i = 0; i < i1; i++) {
-            if (num1Local == length1) {
-                ansList[i] = nums2[num2Local];
-                num2Local++;
-                continue;
-            }
-            if (num2Local == length2) {
-                ansList[i] = nums1[num1Local];
-                num1Local++;
-                continue;
-            }
-            if (nums1[num1Local] > nums2[num2Local]) {
-                ansList[i] = nums2[num2Local];
-                num2Local++;
-            } else {
-                ansList[i] = nums1[num1Local];
-                num1Local++;
-            }
-        }
-        System.out.println(Arrays.toString(ansList));
-        return isOdd ? (ansList[i1 - 1] + ansList[i1 - 2]) / 2.0 : ansList[i1 - 1];
-    }
-}
+/*
+这里我们需要定义一个函数来在两个有序数组中找到第K个元素，
+下面重点来看如何实现找到第K个元素。
+首先，为了避免产生新的数组从而增加时间复杂度，我们使用两个变量i和j分别来标记数组nums1和nums2的起始位置。
+然后来处理一些边界问题，比如当某一个数组的起始位置大于等于其数组长度时，说明其所有数字均已经被淘汰了，相当于一个空数组了，
+那么实际上就变成了在另一个数组中找数字，
+直接就可以找出来了。
+还有就是如果K=1的话，那么我们只要比较nums1和nums2的起始位置i和j上的数字就可以了。
+难点就在于一般的情况怎么处理？
+因为我们需要在两个有序数组中找到第K个元素，为了加快搜索的速度，我们要使用二分法，对K二分，
+意思是我们需要分别在nums1和nums2中查找第K/2个元素，
+注意这里由于两个数组的长度不定，所以有可能某个数组没有第K/2个数字，
+所以我们需要先检查一下，数组中到底存不存在第K/2个数字，如果存在就取出来，否则就赋值上一个整型最大值。
+如果某个数组没有第K/2个数字，那么我们就淘汰另一个数字的前K/2个数字即可。有没有可能两个数组都不存在第K/2个数字呢，
+这道题里是不可能的，因为我们的K不是任意给的，而是给的m+n的中间值，
+所以必定至少会有一个数组是存在第K/2个数字的。
+最后就是二分法的核心啦，比较这两个数组的第K/2小的数字midVal1和midVal2的大小，如果第一个数组的第K/2个数字小的话，
+那么说明我们要找的数字肯定不在nums1中的前K/2个数字，所以我们可以将其淘汰，将nums1的起始位置向后移动K/2个，并且此时的K也自减去K/2，
+调用递归。
+反之，我们淘汰nums2中的前K/2个数字，并将nums2的起始位置向后移动K/2个，并且此时的K也自减去K/2，调用递归即可。
+ */
